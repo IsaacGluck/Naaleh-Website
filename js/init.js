6c46392e5f414a142b56e774aee5649e1ace076d
IsaacGluck <isaacbgluck@gmail.com>
2015-01-27 18:50:47 -0500

    adding slideshow

--- js/init.js
+++ js/init.js
@@ -2,5 +2,14 @@ skel.init({
   containers: '75%',
   grid: {
     gutters: ['0', '0']
-  }
+  },
+  breakpoints: {
+    large: {
+      media: '(min-width: 769px)',
+    },
+    small: {
+      media: '(max-width: 768px)',
+      containers: '95%'
+    }
+  },
 });
