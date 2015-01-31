b2b356155673041d288ce33974618b7fd7341a9a
IsaacGluck <isaacbgluck@gmail.com>
2015-01-31 13:18:34 -0500

    cleaning

--- js/nav.js
+++ js/nav.js
@@ -2,5 +2,17 @@ var $to_stick = $('#toStick'),
 	when_top = $to_stick.offset().top;
 
 $(window).scroll(function() {
-	$to_stick.toggleClass('sticky', $(window).scrollTop() > when_top,);
+	$to_stick.toggleClass('sticky', $(window).scrollTop() > when_top);
 });
+
+$.backstretch("../Naaleh-Website/img/background.png");
+
+$('.carousel').backstretch([
+	"../Naaleh-Website/img/naaleh.jpg",
+	"../Naaleh-Website/img/naaleh3.jpg", 
+	"../Naaleh-Website/img/naaleh6.jpg", 
+	"../Naaleh-Website/img/naaleh8.jpg", 
+	"../Naaleh-Website/img/naaleh4.jpg", 
+	"../Naaleh-Website/img/naaleh5.jpg", 
+	"../Naaleh-Website/img/naaleh7.jpg" 
+	], {duration: 3000, fade: 1000});
