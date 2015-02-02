c45470c0a288207ae8bcb5c4cd29132061402a6f
IsaacGluck <isaacbgluck@gmail.com>
2015-02-01 23:21:58 -0500

    more additions

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){       
+$(document).ready(function(){
 	var $appear = $('#appear');
 	var	$disappear = $('#disappear');
 	var when_top = $disappear.offset().top;
@@ -6,11 +6,11 @@ $(document).ready(function(){
 	$(window).scroll(function() {
 		// $appear.toggleClass('invisible', $(window).scrollTop() < when_top, 400, 'linear');
 		// $disappear.toggleClass('invisible', $(window).scrollTop() > when_top);
-		if($(this).scrollTop() > $disappear.offset().top - 11) {
-			$appear.fadeIn(100);
+		if($(this).scrollTop() > $disappear.offset().top +27) {
+			$appear.fadeIn(300);
 		}
 		else {
-			$appear.fadeOut(200);
+			$appear.fadeOut(300);
 		}
 	});
 
@@ -18,11 +18,11 @@ $(document).ready(function(){
 
 	$('.carousel').backstretch([
 		"../Naaleh-Website/img/naaleh.jpg",
-		"../Naaleh-Website/img/naaleh3.jpg", 
-		"../Naaleh-Website/img/naaleh6.jpg", 
-		"../Naaleh-Website/img/naaleh8.jpg", 
-		"../Naaleh-Website/img/naaleh4.jpg", 
-		"../Naaleh-Website/img/naaleh5.jpg", 
-		"../Naaleh-Website/img/naaleh7.jpg" 
+		"../Naaleh-Website/img/naaleh3.jpg",
+		"../Naaleh-Website/img/naaleh6.jpg",
+		"../Naaleh-Website/img/naaleh8.jpg",
+		"../Naaleh-Website/img/naaleh4.jpg",
+		"../Naaleh-Website/img/naaleh5.jpg",
+		"../Naaleh-Website/img/naaleh7.jpg"
 		], {duration: 3000, fade: 1000});
 });
