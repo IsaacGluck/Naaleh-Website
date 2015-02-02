0500ad8f5b6ac196420d096c3e49be355f2312f8
IsaacGluck <isaacbgluck@gmail.com>
2015-02-01 21:29:43 -0500

    z issues

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,28 @@
-var $to_stick = $('#toStick'),
-	when_top = $to_stick.offset().top;
+$(document).ready(function(){       
+	var $appear = $('#appear');
+	var	$disappear = $('#disappear');
+	var when_top = $disappear.offset().top;
 
-$(window).scroll(function() {
-	$to_stick.toggleClass('sticky', $(window).scrollTop() > when_top);
+	$(window).scroll(function() {
+		// $appear.toggleClass('invisible', $(window).scrollTop() < when_top, 400, 'linear');
+		// $disappear.toggleClass('invisible', $(window).scrollTop() > when_top);
+		if($(this).scrollTop() > $disappear.offset().top + 27) {
+			$appear.fadeIn('100');
+		}
+		else {
+			$appear.fadeOut('200');
+		}
+	});
+
+	$.backstretch("../Naaleh-Website/img/background.png");
+
+	$('.carousel').backstretch([
+		"../Naaleh-Website/img/naaleh.jpg",
+		"../Naaleh-Website/img/naaleh3.jpg", 
+		"../Naaleh-Website/img/naaleh6.jpg", 
+		"../Naaleh-Website/img/naaleh8.jpg", 
+		"../Naaleh-Website/img/naaleh4.jpg", 
+		"../Naaleh-Website/img/naaleh5.jpg", 
+		"../Naaleh-Website/img/naaleh7.jpg" 
+		], {duration: 3000, fade: 1000});
 });
-
-$.backstretch("../Naaleh-Website/img/background.png");
-
-$('.carousel').backstretch([
-	"../Naaleh-Website/img/naaleh.jpg",
-	"../Naaleh-Website/img/naaleh3.jpg", 
-	"../Naaleh-Website/img/naaleh6.jpg", 
-	"../Naaleh-Website/img/naaleh8.jpg", 
-	"../Naaleh-Website/img/naaleh4.jpg", 
-	"../Naaleh-Website/img/naaleh5.jpg", 
-	"../Naaleh-Website/img/naaleh7.jpg" 
-	], {duration: 3000, fade: 1000});
